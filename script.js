9d4dba2cfda3a8d1386d5bf5c31c2db6d23679c5
Sarah Anderson <sarahastephenson15@outlook.com>
2025-08-11 11:07:39 +0100

    fixes made to cart

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", ()=>{
     function cart(){
         let cartCount = 0;
         let cartTotal = 0;
-        
+
         const cartCount1 = document.getElementById("cart-count");
         const cartTotal1 = document.getElementById("cart-totals");
         const buttons = document.querySelectorAll(".add-to-cart");
 
-        buttons.forEach((btn) =>{
-            const product1 = btn.closest(".product");
-            if (!product1(price)) return;
+        buttons.forEach((btn) => {
+            btn.addEventListener("click", () =>{
+                const product1 = btn.closest(".product");
+                const priceString = product1.getAttribute("data-price") || "0";
+                const price = parseFloat(priceString);
+                if (isNaN(price)) return;
 
-            const priceString = product1.getAttribute("data-price") || "0";
-            const price = pareFloat(priceString);
-            if (isNaN(price)) return;
+                cartCount += 1;
+                cartTotal += price;
 
-            cartCount += 1;
-            cartTotal += price;
-
-            cartCount1.textContent = cartCount;
-            cartTotal1.textContent = cartTotal;
-
-        })
+                cartCount1.textContent = cartCount;
+                cartTotal1.textContent = cartTotal.toFixed(2);
+            });
+        });
     }
+    cart();
 })
-cart();
